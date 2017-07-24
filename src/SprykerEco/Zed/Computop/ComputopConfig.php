c3ef3e4c7fc21e2aaab1a7c31e72f0c7d6213a76
laktionova <helen.laktionova@spryker.com>
2017-07-24 17:43:45 +0300

    ECO-665: Add Simple State Machine

---
+++ src/SprykerEco/Zed/Computop/ComputopConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Computop;
+
+use Spryker\Zed\Kernel\AbstractBundleConfig;
+
+class ComputopConfig extends AbstractBundleConfig
+{
+
+    const PROVIDER_NAME = 'Computop';
+    const PAYMENT_METHOD_CREDIT_CARD = 'ComputopCreditCard';
+
+}
