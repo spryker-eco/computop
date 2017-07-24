32a955020e641357d2fdcad20b18003282102930
laktionova <helen.laktionova@spryker.com>
2017-07-24 18:46:26 +0300

    ECO-664: Add Empty Commands and conditions

---
+++ src/SprykerEco/Zed/Computop/Communication/Plugin/Oms/Condition/IsRefundedPlugin.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Computop\Communication\Plugin\Oms\Condition;
+
+use Orm\Zed\Sales\Persistence\SpySalesOrderItem;
+use Spryker\Zed\Oms\Dependency\Plugin\Condition\ConditionInterface;
+
+class IsRefundedPlugin implements ConditionInterface
+{
+
+    /**
+     * @api
+     *
+     * @param \Orm\Zed\Sales\Persistence\SpySalesOrderItem $orderItem
+     *
+     * @return bool
+     */
+    public function check(SpySalesOrderItem $orderItem)
+    {
+        //Todo:implement
+    }
+
+}
