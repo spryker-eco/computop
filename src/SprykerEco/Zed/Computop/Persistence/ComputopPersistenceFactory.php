2e9607218429c990ba81e300876ccf8c956e38d0
laktionova <helen.laktionova@spryker.com>
2017-07-26 16:59:49 +0300

    ECO-670: Add empty persistence layer

---
+++ src/SprykerEco/Zed/Computop/Persistence/ComputopPersistenceFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Computop\Persistence;
+
+use Spryker\Zed\Kernel\Persistence\AbstractPersistenceFactory;
+
+/**
+ * @method \SprykerEco\Zed\Computop\ComputopConfig getConfig()
+ * @method \SprykerEco\Zed\Computop\Persistence\ComputopQueryContainerInterface getQueryContainer()
+ */
+class ComputopPersistenceFactory extends AbstractPersistenceFactory
+{
+}
---
+++ src/SprykerEco/Zed/Computop/Persistence/ComputopQueryContainer.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Computop\Persistence;
+
+use Spryker\Zed\Kernel\Persistence\AbstractQueryContainer;
+
+/**
+ * @method \SprykerEco\Zed\Computop\Persistence\ComputopPersistenceFactory getFactory()
+ */
+class ComputopQueryContainer extends AbstractQueryContainer implements ComputopQueryContainerInterface
+{
+}
