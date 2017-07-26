e05f38083f487714d2fc86411197ddb85f6d6f6a
laktionova <helen.laktionova@spryker.com>
2017-07-26 13:50:52 +0300

    ECO-663: Decrypt failure action

--- src/SprykerEco/Yves/Computop/Controller/CallbackController.php
+++ src/SprykerEco/Yves/Computop/Controller/CallbackController.php
@@ -8,7 +8,9 @@
 namespace SprykerEco\Yves\Computop\Controller;
 
 use Pyz\Yves\Checkout\Plugin\Provider\CheckoutControllerProvider;
+use Spryker\Shared\Config\Config;
 use Spryker\Yves\Kernel\Controller\AbstractController;
+use SprykerEco\Shared\Computop\ComputopConstants;
 
 class CallbackController extends AbstractController
 {
@@ -32,18 +34,41 @@ class CallbackController extends AbstractController
      */
     public function failureAction()
     {
-        $responcseArray = $this->getApplication()['request']->query->all();
+        $responseDecryptedArray = $this->getResponseDecryptedArray();
 
         $errorMessageText = self::ERROR_MESSAGE;
 
         //Add error message + redirect
-        if ($responcseArray['Description'] && $responcseArray['Code']) {
-            $errorMessageText = $responcseArray['Description'] . ' (Code - ' . $responcseArray['Code'] . ')';
+        if ($responseDecryptedArray['Description'] && $responseDecryptedArray['Code']) {
+            $errorMessageText .= ' (' . $responseDecryptedArray['Description'] . ' | ' . $responseDecryptedArray['Code'] . ')';
         }
 
         $this->addErrorMessage($errorMessageText);
 
         return $this->redirectResponseInternal(CheckoutControllerProvider::CHECKOUT_PAYMENT);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getResponseDecryptedArray()
+    {
+        $responseArray = $this->getApplication()['request']->query->all();
+
+        if ($responseArray['Data'] !== null) {
+            $responseDecryptedString = $this->getFactory()->createComputopService()->blowfishDecryptedValue(
+                $responseArray['Data'],
+                $responseArray['Len'],
+                Config::get(ComputopConstants::COMPUTOP_BLOWFISH_PASSWORD)
+            );
+            $responseDecrypted = explode('&', $responseDecryptedString);
+            foreach ($responseDecrypted as $value) {
+                $data = explode('=', $value);
+                $responseArray[array_shift($data)] = array_shift($data);
+            }
+        }
+
+        return $responseArray;
     }
 
 }
