a50cd402e91dfad9998a9013b74e0b63430e533a
laktionova <helen.laktionova@spryker.com>
2017-08-01 19:26:33 +0300

    ECO-666: Add Service to dependency.

---
+++ src/SprykerEco/Zed/Computop/Dependency/Facade/ComputopToComputopServiceBridge.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Computop\Dependency\Facade;
+
+use Generated\Shared\Transfer\ComputopCreditCardPaymentTransfer;
+use SprykerEco\Service\Computop\ComputopServiceInterface;
+
+class ComputopToComputopServiceBridge implements ComputopToComputopServiceInterface
+{
+
+    /**
+     * @var \SprykerEco\Service\Computop\ComputopServiceInterface
+     */
+    protected $computopService;
+
+    /**
+     * @param \SprykerEco\Service\Computop\ComputopServiceInterface $computopService
+     */
+    public function __construct(ComputopServiceInterface $computopService)
+    {
+        $this->computopService = $computopService;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ComputopCreditCardPaymentTransfer $cardPaymentTransfer
+     *
+     * @return string
+     */
+    public function computopMacHashHmacValue(ComputopCreditCardPaymentTransfer $cardPaymentTransfer)
+    {
+        return $this->computopService->computopMacHashHmacValue($cardPaymentTransfer);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ComputopCreditCardPaymentTransfer $cardPaymentTransfer
+     *
+     * @return string
+     */
+    public function computopDataEncryptedValue(ComputopCreditCardPaymentTransfer $cardPaymentTransfer)
+    {
+        return $this->computopService->computopDataEncryptedValue($cardPaymentTransfer);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ComputopCreditCardPaymentTransfer $cardPaymentTransfer
+     *
+     * @return string
+     */
+    public function computopAuthorizationDataEncryptedValue(ComputopCreditCardPaymentTransfer $cardPaymentTransfer)
+    {
+        return $this->computopService->computopAuthorizationDataEncryptedValue($cardPaymentTransfer);
+    }
+
+    /**
+     * @param string $plaintext
+     * @param int $len
+     * @param string $password
+     *
+     * @return string
+     */
+    public function blowfishEncryptedValue($plaintext, $len, $password)
+    {
+        return $this->computopService->blowfishEncryptedValue($plaintext, $len, $password);
+    }
+
+    /**
+     * @param string $cipher
+     * @param int $len
+     * @param string $password
+     *
+     * @return string
+     */
+    public function blowfishDecryptedValue($cipher, $len, $password)
+    {
+        return $this->computopService->blowfishDecryptedValue($cipher, $len, $password);
+    }
+
+}
---
+++ src/SprykerEco/Zed/Computop/Dependency/Facade/ComputopToComputopServiceInterface.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Computop\Dependency\Facade;
+
+use Generated\Shared\Transfer\ComputopCreditCardPaymentTransfer;
+
+interface ComputopToComputopServiceInterface
+{
+
+    /**
+     * @param \Generated\Shared\Transfer\ComputopCreditCardPaymentTransfer $cardPaymentTransfer
+     *
+     * @return string
+     */
+    public function computopMacHashHmacValue(ComputopCreditCardPaymentTransfer $cardPaymentTransfer);
+
+    /**
+     * @param \Generated\Shared\Transfer\ComputopCreditCardPaymentTransfer $cardPaymentTransfer
+     *
+     * @return string
+     */
+    public function computopDataEncryptedValue(ComputopCreditCardPaymentTransfer $cardPaymentTransfer);
+
+    /**
+     * @param \Generated\Shared\Transfer\ComputopCreditCardPaymentTransfer $cardPaymentTransfer
+     *
+     * @return string
+     */
+    public function computopAuthorizationDataEncryptedValue(ComputopCreditCardPaymentTransfer $cardPaymentTransfer);
+
+    /**
+     * @param string $plaintext
+     * @param int $len
+     * @param string $password
+     *
+     * @return string
+     */
+    public function blowfishEncryptedValue($plaintext, $len, $password);
+
+    /**
+     * @param string $cipher
+     * @param int $len
+     * @param string $password
+     *
+     * @return string
+     */
+    public function blowfishDecryptedValue($cipher, $len, $password);
+
+}
