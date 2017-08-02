c80a5ec92b9a9bbea9374409f07d5fd7ab5a8336
laktionova <helen.laktionova@spryker.com>
2017-08-02 13:26:06 +0300

    ECO-667: Remove debug from mapper

--- src/SprykerEco/Zed/Computop/Business/Payment/Mapper/CreditCard/CreditCardMapper.php
+++ src/SprykerEco/Zed/Computop/Business/Payment/Mapper/CreditCard/CreditCardMapper.php
@@ -86,13 +86,6 @@ class CreditCardMapper extends AbstractMapper implements CreditCardMapperInterfa
      */
     protected function getLen(ComputopCreditCardPaymentTransfer $computopCreditCardPaymentTransfer)
     {
-        $responseArray = [
-            'Data' => 'A23501461AA31D2A7DE3ABDBAB085C69369333A28FB75BB52BB2AEBACD1757B834FEF16E8766EF3BA4996A144A34B922A204D24717E46C37811E19511997B96D7601F807CD57A11171922F5D33B696409B81744C558C11BB60BB7D5B21480B692576055F464E83C62D7808994DF4591673B87E7726C69349D5658C52FE7133C016ED4C1129F4D1F499EEE4BA68D85935FA765B84483ED5E4723DF55F56ABD3075C99D1AD05287DFA16CF9C839691CC843E5151469271DF21',
-            'Len' => 178,
-        ];
-        dump($this->getDecryptedArray($responseArray));
-        die;
-
         return strlen($this->computopService->computopAuthorizationDataEncryptedValue($computopCreditCardPaymentTransfer));
     }
 
