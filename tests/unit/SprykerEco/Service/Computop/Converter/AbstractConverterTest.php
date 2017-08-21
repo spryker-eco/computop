3fbb9cdfbc5c2f6e2ec664cacf9f1588d8357f10
laktionova <helen.laktionova@spryker.com>
2017-08-21 16:02:44 +0300

    ECO-695: Add unit test (service converter)

---
+++ tests/unit/SprykerEco/Service/Computop/Converter/AbstractConverterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Unit\SprykerEco\Service\Computop\Converter;
+
+use Codeception\TestCase\Test;
+use SprykerEco\Service\Computop\Model\Converter\Computop;
+
+abstract class AbstractConverterTest extends Test
+{
+    /**
+     * @return \SprykerEco\Service\Computop\Model\Converter\Computop
+     */
+    protected function createConverter()
+    {
+        return new Computop();
+    }
+}
---
+++ tests/unit/SprykerEco/Service/Computop/Converter/ComputopConverterTest.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Unit\SprykerEco\Service\Computop\Converter;
+
+use Generated\Shared\Transfer\ComputopResponseHeaderTransfer;
+use SprykerEco\Service\Computop\Exception\ComputopConverterException;
+use SprykerEco\Shared\Computop\ComputopConstants;
+
+/**
+ * @group Unit
+ * @group SprykerEco
+ * @group Service
+ * @group Computop
+ * @group Api
+ * @group Converter
+ * @group ComputopConverterTest
+ */
+class ComputopConverterTest extends AbstractConverterTest
+{
+
+    const METHOD = 'AUTHORIZE';
+
+    const PAY_ID_VALUE = 'PAY_ID_VALUE';
+    const TRANS_ID_VALUE = 'TRANS_ID_VALUE';
+    const MERCHANT_ID_VALUE = 'MERCHANT_ID_VALUE';
+    const X_ID_VALUE = 'X_ID_VALUE';
+    const CODE_VALUE = '000000';
+    const STATUS_ERROR_VALUE = 'ERROR';
+
+
+    /**
+     * @return void
+     */
+    public function testExtractIsSuccessHeader()
+    {
+        $converter = $this->createConverter();
+        $decryptedArray = $this->getDecryptedArray(ComputopConstants::SUCCESS_STATUS);
+
+        /** @var  \Generated\Shared\Transfer\ComputopResponseHeaderTransfer $header */
+        $header = $converter->extractHeader($decryptedArray, self::METHOD);
+
+        $this->assertInstanceOf(ComputopResponseHeaderTransfer::class, $header);
+        $this->assertTrue($header->getIsSuccess());
+
+        $this->assertSame(self::MERCHANT_ID_VALUE, $header->getMId());
+        $this->assertSame(self::TRANS_ID_VALUE, $header->getTransId());
+        $this->assertSame(self::PAY_ID_VALUE, $header->getPayId());
+        $this->assertSame(ComputopConstants::SUCCESS_STATUS, $header->getStatus());
+        $this->assertSame(self::CODE_VALUE, $header->getCode());
+        $this->assertSame(self::X_ID_VALUE, $header->getXId());
+    }
+
+
+    /**
+     * @return void
+     */
+    public function testExtractIsErrorHeader()
+    {
+        $converter = $this->createConverter();
+        $decryptedArray = $this->getDecryptedArray(self::STATUS_ERROR_VALUE);
+
+        /** @var  \Generated\Shared\Transfer\ComputopResponseHeaderTransfer $header */
+        $header = $converter->extractHeader($decryptedArray, self::METHOD);
+
+        $this->assertInstanceOf(ComputopResponseHeaderTransfer::class, $header);
+        $this->assertFalse($header->getIsSuccess());
+
+        $this->assertSame(self::MERCHANT_ID_VALUE, $header->getMId());
+        $this->assertSame(self::TRANS_ID_VALUE, $header->getTransId());
+        $this->assertSame(self::PAY_ID_VALUE, $header->getPayId());
+        $this->assertSame(self::STATUS_ERROR_VALUE, $header->getStatus());
+        $this->assertSame(self::CODE_VALUE, $header->getCode());
+        $this->assertSame(self::X_ID_VALUE, $header->getXId());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCheckEncryptedResponse()
+    {
+        $converter = $this->createConverter();
+
+        $responseArray = [
+            ComputopConstants::DATA_F_N => 'data',
+            ComputopConstants::LEN_F_N => 4,
+        ];
+
+        $converter->checkEncryptedResponse($responseArray);
+    }
+
+    /**
+     * @return void
+     */
+    public function testCheckEncryptedResponseError()
+    {
+        $this->expectException(ComputopConverterException::class);
+
+        $converter = $this->createConverter();
+        $converter->checkEncryptedResponse([]);
+    }
+
+    /**
+     * @param string $status
+     *
+     * @return array
+     */
+    protected function getDecryptedArray($status)
+    {
+        $decryptedArray = [
+            ComputopConstants::MID_F_N => self::MERCHANT_ID_VALUE,
+            ComputopConstants::TRANS_ID_F_N => self::TRANS_ID_VALUE,
+            ComputopConstants::PAY_ID_F_N => self::PAY_ID_VALUE,
+            ComputopConstants::STATUS_F_N => $status,
+            ComputopConstants::CODE_F_N => self::CODE_VALUE,
+            ComputopConstants::XID_F_N => self::X_ID_VALUE,
+        ];
+
+        return $decryptedArray;
+    }
+
+}
