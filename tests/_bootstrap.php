07432a4b0044af1889a38428c78ab2b4dd20f209
laktionova <helen.laktionova@spryker.com>
2017-09-01 12:54:16 +0300

    Code review: update tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,6 +1,1 @@
 <?php
-defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'devtest');
-defined('APPLICATION_STORE') || define('APPLICATION_STORE', 'DE');
-defined('APPLICATION_ROOT_DIR') || define('APPLICATION_ROOT_DIR',  '/data/shop/development/current');
-defined('APPLICATION') || define('APPLICATION', 'ZED');
-defined('APPLICATION_VENDOR_DIR') || define('APPLICATION_VENDOR_DIR',  realpath(__DIR__));
---
+++ tests/_support/Module/EnvironmentModule.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Computop\Module;
+
+use Codeception\Lib\ModuleContainer;
+use SprykerTest\Shared\Testify\Helper\Environment;
+
+class EnvironmentModule extends Environment
+{
+
+    /**
+     * @inheritdoc
+     *
+     * @param \Codeception\Lib\ModuleContainer $moduleContainer
+     * @param array|null $config
+     */
+    public function __construct(ModuleContainer $moduleContainer, $config = null)
+    {
+        $this->_initialize();
+        parent::__construct($moduleContainer, $config);
+    }
+
+}
