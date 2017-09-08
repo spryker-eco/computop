57a8848929cdb1579bd22179a878ed8d2c4b0d6a
laktionova <helen.laktionova@spryker.com>
2017-09-08 16:34:08 +0300

    ECO-736: update transfer and configs

--- config/Zed/config.dist.php
+++ config/Zed/config.dist.php
@@ -10,11 +10,13 @@ $config[ComputopConstants::COMPUTOP_BLOWFISH_PASSWORD] = 'COMPUTOP_BLOWFISH_PASS
 $config[ComputopConstants::COMPUTOP_HMAC_PASSWORD] = 'COMPUTOP_HMAC_PASSWORD'; //Set up real data
 
 $config[ComputopConstants::COMPUTOP_CREDIT_CARD_ORDER_ACTION] = 'https://www.computop-paygate.com/payssl.aspx';
-$config[ComputopConstants::COMPUTOP_CREDIT_CARD_AUTHORIZE_ACTION] = 'https://www.computop-paygate.com/authorize.aspx';
-$config[ComputopConstants::COMPUTOP_CREDIT_CARD_CAPTURE_ACTION] = 'https://www.computop-paygate.com/capture.aspx';
-$config[ComputopConstants::COMPUTOP_CREDIT_CARD_REVERSE_ACTION] = 'https://www.computop-paygate.com/reverse.aspx';
-$config[ComputopConstants::COMPUTOP_CREDIT_CARD_INQUIRE_ACTION] = 'https://www.computop-paygate.com/inquire.aspx';
-$config[ComputopConstants::COMPUTOP_CREDIT_CARD_REFUND_ACTION] = 'https://www.computop-paygate.com/credit.aspx';
+$config[ComputopConstants::COMPUTOP_AUTHORIZE_ACTION] = 'https://www.computop-paygate.com/authorize.aspx';
+$config[ComputopConstants::COMPUTOP_CAPTURE_ACTION] = 'https://www.computop-paygate.com/capture.aspx';
+$config[ComputopConstants::COMPUTOP_REVERSE_ACTION] = 'https://www.computop-paygate.com/reverse.aspx';
+$config[ComputopConstants::COMPUTOP_INQUIRE_ACTION] = 'https://www.computop-paygate.com/inquire.aspx';
+$config[ComputopConstants::COMPUTOP_REFUND_ACTION] = 'https://www.computop-paygate.com/credit.aspx';
+
+$config[ComputopConstants::COMPUTOP_PAY_PAL_ORDER_ACTION] = 'https://www.computop-paygate.com/paypal.aspx';
 
 $config[ComputopConstants::COMPUTOP_RESPONSE_MAC_REQUIRED] = [
     ComputopConstants::ORDER_METHOD,
