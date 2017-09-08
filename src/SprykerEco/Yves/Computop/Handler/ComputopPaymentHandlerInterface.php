8512b7b9b302b56ca262e6e9c05c5c60a3a7329d
laktionova <helen.laktionova@spryker.com>
2017-09-08 16:53:06 +0300

    ECO-745: add ComputopPaymentHandlerInterface

---
+++ src/SprykerEco/Yves/Computop/Handler/ComputopPaymentHandlerInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Yves\Computop\Handler;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
+
+interface ComputopPaymentHandlerInterface
+{
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     * @param \Spryker\Shared\Kernel\Transfer\AbstractTransfer $responseTransfer
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function addPaymentToQuote(QuoteTransfer $quoteTransfer, AbstractTransfer $responseTransfer);
+
+}
