29e21ba88435aa0f72fff2b0c38d763249e3ff6c
laktionova <helen.laktionova@spryker.com>
2017-11-01 13:04:03 +0200

    Refactoring: Update FormDataProviders

--- src/SprykerEco/Yves/Computop/Form/DataProvider/AbstractFormDataProvider.php
+++ src/SprykerEco/Yves/Computop/Form/DataProvider/AbstractFormDataProvider.php
@@ -9,6 +9,8 @@ namespace SprykerEco\Yves\Computop\Form\DataProvider;
 
 use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
 use Spryker\Yves\StepEngine\Dependency\Form\StepEngineFormDataProviderInterface;
+use SprykerEco\Yves\Computop\Dependency\Client\ComputopToQuoteInterface;
+use SprykerEco\Yves\Computop\Mapper\Order\MapperInterface;
 
 abstract class AbstractFormDataProvider implements StepEngineFormDataProviderInterface
 {
@@ -18,6 +20,36 @@ abstract class AbstractFormDataProvider implements StepEngineFormDataProviderInt
      * @return \Spryker\Shared\Kernel\Transfer\TransferInterface
      */
     abstract protected function getComputopPayment(AbstractTransfer $quoteTransfer);
+
+    /**
+     * @var \SprykerEco\Yves\Computop\Dependency\Client\ComputopToQuoteInterface
+     */
+    protected $quoteClient;
+
+    /**
+     * @var \SprykerEco\Yves\Computop\Mapper\Order\MapperInterface
+     */
+    protected $cardMapper;
+
+    /**
+     * @param \SprykerEco\Yves\Computop\Dependency\Client\ComputopToQuoteInterface $quoteClient
+     * @param \SprykerEco\Yves\Computop\Mapper\Order\MapperInterface $cardMapper
+     */
+    public function __construct(ComputopToQuoteInterface $quoteClient, MapperInterface $cardMapper)
+    {
+        $this->quoteClient = $quoteClient;
+        $this->cardMapper = $cardMapper;
+    }
+
+    /**
+     * @param \Spryker\Shared\Kernel\Transfer\AbstractTransfer|\Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return array
+     */
+    public function getOptions(AbstractTransfer $quoteTransfer)
+    {
+        return [];
+    }
 
     /**
      * @param \Spryker\Shared\Kernel\Transfer\AbstractTransfer|\Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
--- src/SprykerEco/Yves/Computop/Form/DataProvider/SofortFormDataProvider.php
+++ src/SprykerEco/Yves/Computop/Form/DataProvider/SofortFormDataProvider.php
@@ -9,31 +9,9 @@ namespace SprykerEco\Yves\Computop\Form\DataProvider;
 
 use Generated\Shared\Transfer\PaymentTransfer;
 use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
-use SprykerEco\Yves\Computop\Dependency\Client\ComputopToQuoteInterface;
-use SprykerEco\Yves\Computop\Mapper\Order\MapperInterface;
 
 class SofortFormDataProvider extends AbstractFormDataProvider
 {
-    /**
-     * @var \SprykerEco\Yves\Computop\Dependency\Client\ComputopToQuoteInterface
-     */
-    protected $quoteClient;
-
-    /**
-     * @var \SprykerEco\Yves\Computop\Mapper\Order\MapperInterface
-     */
-    protected $cardMapper;
-
-    /**
-     * @param \SprykerEco\Yves\Computop\Dependency\Client\ComputopToQuoteInterface $quoteClient
-     * @param \SprykerEco\Yves\Computop\Mapper\Order\MapperInterface $cardMapper
-     */
-    public function __construct(ComputopToQuoteInterface $quoteClient, MapperInterface $cardMapper)
-    {
-        $this->quoteClient = $quoteClient;
-        $this->cardMapper = $cardMapper;
-    }
-
     /**
      * @param \Spryker\Shared\Kernel\Transfer\AbstractTransfer|\Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
      *
@@ -57,16 +35,6 @@ class SofortFormDataProvider extends AbstractFormDataProvider
         }
 
         return $quoteTransfer;
-    }
-
-    /**
-     * @param \Spryker\Shared\Kernel\Transfer\AbstractTransfer|\Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
-     *
-     * @return array
-     */
-    public function getOptions(AbstractTransfer $quoteTransfer)
-    {
-        return [];
     }
 
     /**
