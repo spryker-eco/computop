af50428ee4f12ebccb40f9bfc64a340cd3280947
laktionova <helen.laktionova@spryker.com>
2017-11-15 11:47:20 +0200

    feature/eco-982-easy-credit * ECO-991: Create Yves mapper for EasyCredit payment method form

---
+++ src/SprykerEco/Yves/Computop/Mapper/Init/PrePlace/EasyCreditMapper.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Yves\Computop\Mapper\Init\PrePlace;
+
+use Generated\Shared\Transfer\ComputopEasyCreditPaymentTransfer;
+use Spryker\Shared\Kernel\Transfer\TransferInterface;
+use SprykerEco\Shared\Computop\Config\ComputopApiConfig;
+use SprykerEco\Yves\Computop\Plugin\Provider\ComputopControllerProvider;
+
+class EasyCreditMapper extends AbstractPrePlaceMapper
+{
+    /**
+     * @param \Spryker\Shared\Kernel\Transfer\TransferInterface|\Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\ComputopEasyCreditPaymentTransfer
+     */
+    protected function createTransferWithUnencryptedValues(TransferInterface $quoteTransfer)
+    {
+        $computopPaymentTransfer = new ComputopEasyCreditPaymentTransfer();
+        
+        $computopPaymentTransfer->setTransId($this->generateTransId($quoteTransfer));
+        $computopPaymentTransfer->setUrlSuccess(
+            $this->getAbsoluteUrl($this->application->path(ComputopControllerProvider::EASY_CREDIT_SUCCESS))
+        );
+
+        return $computopPaymentTransfer;
+    }
+
+    /**
+     * @param \Spryker\Shared\Kernel\Transfer\TransferInterface $cardPaymentTransfer
+     *
+     * @return array
+     */
+    protected function getDataSubArray(TransferInterface $cardPaymentTransfer)
+    {
+        /** @var \Generated\Shared\Transfer\ComputopEasyCreditPaymentTransfer $cardPaymentTransfer */
+        $dataSubArray[ComputopApiConfig::TRANS_ID] = $cardPaymentTransfer->getTransId();
+        $dataSubArray[ComputopApiConfig::AMOUNT] = $cardPaymentTransfer->getAmount();
+        $dataSubArray[ComputopApiConfig::CURRENCY] = $cardPaymentTransfer->getCurrency();
+        $dataSubArray[ComputopApiConfig::URL_SUCCESS] = $cardPaymentTransfer->getUrlSuccess();
+        $dataSubArray[ComputopApiConfig::URL_FAILURE] = $cardPaymentTransfer->getUrlFailure();
+        $dataSubArray[ComputopApiConfig::RESPONSE] = $cardPaymentTransfer->getResponse();
+
+        return $dataSubArray;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getActionUrl()
+    {
+        return $this->config->getEasyCreditInitAction();
+    }
+}
