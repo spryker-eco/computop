7d13448473e05d7599f8615e1697417e2e49a143
laktionova <helen.laktionova@spryker.com>
2017-11-15 15:15:08 +0200

    Refactoring * Servise. Config interface is added.

---
+++ src/SprykerEco/Service/Computop/ComputopConfigInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Service\Computop;
+
+interface ComputopConfigInterface
+{
+    /**
+     * @param string $method
+     *
+     * @return bool
+     */
+    public function isMacRequired($method);
+
+    /**
+     * @return string
+     */
+    public function getHmacPassword();
+
+    /**
+     * @return bool
+     */
+    public function isTestMode();
+}
