96cef3e22a4b5521788151af515bf922f9f14560
Aleksey Kotsuba <aleksey.kotsuba@spryker.com>
2018-06-14 12:29:06 +0000

    ECO-1574 Add missing facade tests.

--- tests/SprykerEcoTest/Zed/Computop/_support/Order/OrderPaymentTestHelper.php
+++ tests/SprykerEcoTest/Zed/Computop/_support/Order/OrderPaymentTestHelper.php
@@ -26,7 +26,7 @@ use SprykerEco\Zed\Computop\Persistence\ComputopQueryContainer;
 class OrderPaymentTestHelper extends Test
 {
     const CURRENCY_VALUE = 'USD';
-    const DATA_VALUE = 'DATA_VALUE';
+    const DATA_VALUE = 'f0957c5d3799a902211fb2e019cef4f459f29d4d908ffc66e6aa0fb839023c62be996fc15b44bcd681ce7bb5ff988c7157707228fb13b961fd676abedef6aeed48817998d16a635e11943e70942b91751a5793ce85a43e6a38220eae6bdbb594f3dcfb398e2f8f9a1d9aa922a05821950ff5dbae45eaf9a5818fe75ad0a8e246df61165cf5fe6727b922edf08e6d7b4004746be1259a9c6d';
     const LEN_VALUE = 10;
 
     /**
