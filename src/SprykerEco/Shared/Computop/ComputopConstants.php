5f3bb612d89756067364d7f665a92307d27dac50
Aleksey Kotsuba <aleksey.kotsuba@spryker.com>
2018-08-22 11:22:00 +0000

    ECO-1927: Fix phpcs.

--- src/SprykerEco/Shared/Computop/ComputopConstants.php
+++ src/SprykerEco/Shared/Computop/ComputopConstants.php
@@ -7,6 +7,9 @@
 
 namespace SprykerEco\Shared\Computop;
 
+/**
+ * Declares global environment configuration keys. Do not use it for other class constants.
+ */
 interface ComputopConstants
 {
     public const HMAC_PASSWORD = 'COMPUTOP:HMAC_PASSWORD';
