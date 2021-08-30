8fe646b1f9b480fe0aee38dd8391b50b213a412a
Roman Sadkivskyi <roman.sadkivskyi@spryker.com>
2021-08-30 15:40:29 +0300

    ECO-3744 - Fixed CodeStyle issues.

--- src/SprykerEco/Zed/Computop/Business/Payment/Handler/Saver/Init/PayuCeeSingleResponseSaver.php
+++ src/SprykerEco/Zed/Computop/Business/Payment/Handler/Saver/Init/PayuCeeSingleResponseSaver.php
@@ -92,7 +92,7 @@ class PayuCeeSingleResponseSaver extends AbstractResponseSaver
     protected function getPaymentStatus(ComputopPayuCeeSingleInitResponseTransfer $responseTransfer): string
     {
         $responseStatus = $this->getResponseStatus($responseTransfer);
-        if ($responseTransfer === null) {
+        if ($responseStatus === null) {
             return $this->config->getOmsStatusNew();
         }
 
