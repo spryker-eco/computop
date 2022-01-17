91732f6fbbc89b03b15b1ccdf6bbc1efda0779df
Michael Knappe <michael.knappe@spryker.com>
2022-01-17 22:29:53 +0100

    Fixed call to an undefined method in InitPaydirektMapper::getDataSubArray()

--- src/SprykerEco/Zed/Computop/Business/Hook/Mapper/Init/InitPaydirektMapper.php
+++ src/SprykerEco/Zed/Computop/Business/Hook/Mapper/Init/InitPaydirektMapper.php
@@ -7,6 +7,7 @@
 
 namespace SprykerEco\Zed\Computop\Business\Hook\Mapper\Init;
 
+use Generated\Shared\Transfer\ComputopPaydirektPaymentTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
 use Spryker\Shared\Kernel\Transfer\TransferInterface;
 use SprykerEco\Shared\Computop\ComputopConfig;
@@ -61,11 +62,11 @@ class InitPaydirektMapper extends AbstractMapper
     }
 
     /**
-     * @param \Spryker\Shared\Kernel\Transfer\TransferInterface $computopPaydirectPaymentTransfer
+     * @param \Generated\Shared\Transfer\ComputopPaydirektPaymentTransfer $computopPaydirectPaymentTransfer
      *
      * @return array
      */
-    protected function getDataSubArray(TransferInterface $computopPaydirectPaymentTransfer): array
+    protected function getDataSubArray(ComputopPaydirektPaymentTransfer $computopPaydirectPaymentTransfer): array
     {
         return [
             ComputopApiConfig::TRANS_ID => $computopPaydirectPaymentTransfer->getTransId(),
